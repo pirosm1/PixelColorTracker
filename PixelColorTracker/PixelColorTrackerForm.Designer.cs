b286660cd62ad3d39ac846be2b8a2d9e5578d6e2
Mnemeth Piros <mnemethp@gmail.com>
2019-02-06 07:59:43 -0800

    Add project files.

---
+++ PixelColorTracker/PixelColorTrackerForm.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace PixelColorTracker
+{
+    partial class PixelColorTrackerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Load_Image_Button = new System.Windows.Forms.Button();
+            this.Loaded_Image_Box = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label24 = new System.Windows.Forms.Label();
+            this.label25 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Loaded_Image_Box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Load_Image_Button
+            // 
+            this.Load_Image_Button.Location = new System.Drawing.Point(15, 15);
+            this.Load_Image_Button.Margin = new System.Windows.Forms.Padding(6);
+            this.Load_Image_Button.Name = "Load_Image_Button";
+            this.Load_Image_Button.Size = new System.Drawing.Size(548, 64);
+            this.Load_Image_Button.TabIndex = 0;
+            this.Load_Image_Button.Text = "Load Image";
+            this.Load_Image_Button.UseVisualStyleBackColor = true;
+            this.Load_Image_Button.Click += new System.EventHandler(this.Load_Image_Button_Click);
+            // 
+            // Loaded_Image_Box
+            // 
+            this.Loaded_Image_Box.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Loaded_Image_Box.Location = new System.Drawing.Point(572, 15);
+            this.Loaded_Image_Box.Name = "Loaded_Image_Box";
+            this.Loaded_Image_Box.Size = new System.Drawing.Size(905, 898);
+            this.Loaded_Image_Box.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Loaded_Image_Box.TabIndex = 1;
+            this.Loaded_Image_Box.TabStop = false;
+            this.Loaded_Image_Box.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Loaded_Image_Box_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(15, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 100);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(127, 91);
+            this.label2.Margin = new System.Windows.Forms.Padding(6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 100);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(239, 91);
+            this.label3.Margin = new System.Windows.Forms.Padding(6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 100);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(351, 91);
+            this.label4.Margin = new System.Windows.Forms.Padding(6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 100);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Location = new System.Drawing.Point(463, 91);
+            this.label5.Margin = new System.Windows.Forms.Padding(6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 100);
+            this.label5.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(15, 203);
+            this.label6.Margin = new System.Windows.Forms.Padding(6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 100);
+            this.label6.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Location = new System.Drawing.Point(127, 203);
+            this.label7.Margin = new System.Windows.Forms.Padding(6);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 100);
+            this.label7.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Location = new System.Drawing.Point(239, 203);
+            this.label8.Margin = new System.Windows.Forms.Padding(6);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 100);
+            this.label8.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label9.Location = new System.Drawing.Point(351, 203);
+            this.label9.Margin = new System.Windows.Forms.Padding(6);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 100);
+            this.label9.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label10.Location = new System.Drawing.Point(463, 203);
+            this.label10.Margin = new System.Windows.Forms.Padding(6);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 100);
+            this.label10.TabIndex = 11;
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label11.Location = new System.Drawing.Point(15, 315);
+            this.label11.Margin = new System.Windows.Forms.Padding(6);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(100, 100);
+            this.label11.TabIndex = 12;
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label12.Location = new System.Drawing.Point(127, 315);
+            this.label12.Margin = new System.Windows.Forms.Padding(6);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 100);
+            this.label12.TabIndex = 13;
+            // 
+            // label13
+            // 
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label13.Location = new System.Drawing.Point(239, 315);
+            this.label13.Margin = new System.Windows.Forms.Padding(6);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(100, 100);
+            this.label13.TabIndex = 14;
+            // 
+            // label14
+            // 
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label14.Location = new System.Drawing.Point(351, 315);
+            this.label14.Margin = new System.Windows.Forms.Padding(6);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(100, 100);
+            this.label14.TabIndex = 15;
+            // 
+            // label15
+            // 
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label15.Location = new System.Drawing.Point(463, 315);
+            this.label15.Margin = new System.Windows.Forms.Padding(6);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(100, 100);
+            this.label15.TabIndex = 15;
+            // 
+            // label16
+            // 
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label16.Location = new System.Drawing.Point(15, 427);
+            this.label16.Margin = new System.Windows.Forms.Padding(6);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(100, 100);
+            this.label16.TabIndex = 16;
+            // 
+            // label17
+            // 
+            this.label17.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label17.Location = new System.Drawing.Point(127, 427);
+            this.label17.Margin = new System.Windows.Forms.Padding(6);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(100, 100);
+            this.label17.TabIndex = 17;
+            // 
+            // label18
+            // 
+            this.label18.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label18.Location = new System.Drawing.Point(239, 427);
+            this.label18.Margin = new System.Windows.Forms.Padding(6);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(100, 100);
+            this.label18.TabIndex = 18;
+            // 
+            // label19
+            // 
+            this.label19.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label19.Location = new System.Drawing.Point(351, 427);
+            this.label19.Margin = new System.Windows.Forms.Padding(6);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(100, 100);
+            this.label19.TabIndex = 19;
+            // 
+            // label20
+            // 
+            this.label20.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label20.Location = new System.Drawing.Point(463, 427);
+            this.label20.Margin = new System.Windows.Forms.Padding(6);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(100, 100);
+            this.label20.TabIndex = 20;
+            // 
+            // label21
+            // 
+            this.label21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label21.Location = new System.Drawing.Point(15, 539);
+            this.label21.Margin = new System.Windows.Forms.Padding(6);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(100, 100);
+            this.label21.TabIndex = 21;
+            // 
+            // label22
+            // 
+            this.label22.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label22.Location = new System.Drawing.Point(127, 539);
+            this.label22.Margin = new System.Windows.Forms.Padding(6);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(100, 100);
+            this.label22.TabIndex = 22;
+            // 
+            // label23
+            // 
+            this.label23.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label23.Location = new System.Drawing.Point(239, 539);
+            this.label23.Margin = new System.Windows.Forms.Padding(6);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(100, 100);
+            this.label23.TabIndex = 23;
+            // 
+            // label24
+            // 
+            this.label24.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label24.Location = new System.Drawing.Point(351, 539);
+            this.label24.Margin = new System.Windows.Forms.Padding(6);
+            this.label24.Name = "label24";
+            this.label24.Size = new System.Drawing.Size(100, 100);
+            this.label24.TabIndex = 24;
+            // 
+            // label25
+            // 
+            this.label25.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label25.Location = new System.Drawing.Point(463, 539);
+            this.label25.Margin = new System.Windows.Forms.Padding(6);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(100, 100);
+            this.label25.TabIndex = 25;
+            // 
+            // PixelColorTrackerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1482, 922);
+            this.Controls.Add(this.label25);
+            this.Controls.Add(this.label24);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Loaded_Image_Box);
+            this.Controls.Add(this.Load_Image_Button);
+            this.Name = "PixelColorTrackerForm";
+            this.Text = "Pixel Color Tracker";
+            this.Load += new System.EventHandler(this.PixelColorTrackerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Loaded_Image_Box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load_Image_Button;
+        private System.Windows.Forms.PictureBox Loaded_Image_Box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label24;
+        private System.Windows.Forms.Label label25;
+    }
+}
+
---
+++ PixelColorTracker/PixelColorTrackerForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelColorTracker
+{
+    public partial class PixelColorTrackerForm : Form
+    {
+        private Label[][] labels;
+
+        private const int numberOfCols = 5;
+        private const int numberOfRows = 5;
+
+        public PixelColorTrackerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PixelColorTrackerForm_Load(object sender, EventArgs e)
+        {
+            labels = new Label[numberOfCols][];
+
+            for (int x = 0; x < numberOfCols; x++)
+            {
+                labels[x] = new Label[numberOfRows];
+            }
+
+            labels[0][0] = label1;
+            labels[1][0] = label2;
+            labels[2][0] = label3;
+            labels[3][0] = label4;
+            labels[4][0] = label5;
+
+            labels[0][1] = label6;
+            labels[1][1] = label7;
+            labels[2][1] = label8;
+            labels[3][1] = label9;
+            labels[4][1] = label10;
+
+            labels[0][2] = label11;
+            labels[1][2] = label12;
+            labels[2][2] = label13;
+            labels[3][2] = label14;
+            labels[4][2] = label15;
+
+            labels[0][3] = label16;
+            labels[1][3] = label17;
+            labels[2][3] = label18;
+            labels[3][3] = label19;
+            labels[4][3] = label20;
+
+            labels[0][4] = label21;
+            labels[1][4] = label22;
+            labels[2][4] = label23;
+            labels[3][4] = label24;
+            labels[4][4] = label25;
+        }
+
+        private void Load_Image_Button_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openImageDlg = new OpenFileDialog())
+            {
+                openImageDlg.Title = "Load Image";
+                openImageDlg.Filter = "Image Files |*.bmp;*.jpg;*.jpeg;*.gif;*.png";
+
+                if (openImageDlg.ShowDialog() == DialogResult.OK)
+                {
+                    Loaded_Image_Box.Image = new Bitmap(openImageDlg.FileName);
+                }
+            }
+        }
+
+        // Determine pixel color in accordance to W3C recommendations
+        // Determines "relative luminance"
+        private Color Determine_Font_Color(Color pixelColor)
+        {
+            // see https://www.w3.org/TR/WCAG20/relative-luminance.xml
+            // for relative luminance definition
+            double sR = pixelColor.R / 255.0, sG = pixelColor.G / 255.0, sB = pixelColor.B / 255.0;
+
+            if (sR <= 0.03928)
+            {
+                sR /= 12.92;
+            }
+            else
+            {
+                sR = Math.Pow((sR + 0.055) / 1.055, 2.4);
+            }
+            if (sG <= 0.03928)
+            {
+                sG /= 12.92;
+            }
+            else
+            {
+                sG = Math.Pow((sG + 0.055) / 1.055, 2.4);
+            }
+            if (sB <= 0.03928)
+            {
+                sB /= 12.92;
+            }
+            else
+            {
+                sB = Math.Pow((sB + 0.055) / 1.055, 2.4);
+            }
+
+            double L = 0.2126 * sR + 0.7152 * sG + 0.0722 * sB;
+
+            // Recommended contrast ratio is (L1 + 0.05) / (L2 + 0.05)
+            // where L1 is the lighter color and L2 is the darker color
+            // Luminance of black is 0.0, luminance of white is 1.0
+            // so if the contrast between the relative luminance of our color L and black
+            // is greater that of the relative luminance of our color L and white,
+            // then we use black, otherwise we use white.
+            // (L + 0.05) / 0.05 > 1.05 / (L + 0.05)
+            // simpilfies to L > 0.179
+            if (L > 0.179)
+            {
+                return Color.Black;
+            }
+            return Color.White;
+        }
+        
+        private void Loaded_Image_Box_MouseMove(object sender, MouseEventArgs e)
+        {
+            // if the image is not loaded, do nothing
+            if (Loaded_Image_Box.Image == null)
+            {
+                return;
+            }
+            Bitmap loadedImage = (Bitmap)Loaded_Image_Box.Image;
+            Point mouseLocation = e.Location;
+            for (int x = 0; x < numberOfCols; x++)
+            {
+                int colOffset = mouseLocation.X + x - 2;
+                // If the cursor is on the very right or left of the screen,
+                if (colOffset < 0 || colOffset >= Loaded_Image_Box.Image.Width)
+                {
+                    // we can safely throw out the whole column
+                    for (int y = 0; y < numberOfRows; y++)
+                    {
+                        labels[x][y].Text = "";
+                        labels[x][y].BackColor = Color.Black;
+                    }
+                    continue;
+                }
+                for (int y = 0; y < numberOfRows; y++)
+                {
+                    int rowOffset = mouseLocation.Y + y - 2;
+
+                    // if the cursor is on the very top or very bottom of the screen
+                    if (rowOffset < 0 || rowOffset >= Loaded_Image_Box.Image.Height)
+                    {
+                        // we can throw out this cell
+                        labels[x][y].Text = "";
+                        labels[x][y].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        Color pixelColor = loadedImage.GetPixel(colOffset, rowOffset);
+                        labels[x][y].Text = string.Format("({0}, {1}){5}{2}{5}{3}{5}{4}", colOffset, rowOffset, pixelColor.R, pixelColor.G, pixelColor.G, Environment.NewLine);
+                        labels[x][y].BackColor = pixelColor;
+                        labels[x][y].ForeColor = Determine_Font_Color(pixelColor);
+                    }
+                }
+            }
+        }
+    }
+}
